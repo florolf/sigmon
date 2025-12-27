a8ea18b123f622603d9e5df8255eeb187b821d21
Florian Larysch <fl@n621.de>
2025-12-27 15:12:33 +0100

    utils: add base64 encoding helper

--- src/sigmon/utils.py
+++ src/sigmon/utils.py
@@ -5,5 +5,9 @@ def b64enc(data: bytes) -> str:
     return base64.b64encode(data).decode('ascii')
 
 
+def b64dec(text: str) -> bytes:
+    return base64.b64decode(text)
+
+
 def sha256(data: bytes) -> bytes:
     return hashlib.sha256(data).digest()
